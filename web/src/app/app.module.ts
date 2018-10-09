5d8f8fd43852d1bc19cf06fe3adcc7448c75cef2
IncredibleQuark <lkupinski@gmail.com>
2018-10-09 23:22:11 +0200

    Basic AGM map implementation

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MapComponent } from './components/board/map/map.component';
 
+import { AgmCoreModule } from '@agm/core';
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: environment.googleApiKey
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- web/src/app/components/board/map/map.component.ts
+++ web/src/app/components/board/map/map.component.ts
@@ -1,13 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, NgZone, OnInit, ViewChild} from '@angular/core';
+import { MapsAPILoader, AgmMap } from '@agm/core';
+
+interface Marker {
+  lat: number;
+  lng: number;
+  label?: string;
+  draggable: boolean;
+}
+
+interface GMapLocation {
+  lat: number;
+  lng: number;
+  viewport?: Object;
+  zoom: number;
+  address_level_1?:string;
+  address_level_2?: string;
+  address_country?: string;
+  address_zip?: string;
+  address_state?: string;
+  marker?: Marker;
+}
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
+
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  public location:any = {
+    lat: 51.678418,
+    lng: 7.809007,
+    marker: {
+      lat: 51.678418,
+      lng: 7.809007,
+      draggable: true
+    },
+    zoom: 5
+  };
+
+
+  @ViewChild(AgmMap) map: AgmMap;
+
+  constructor(public mapsApiLoader: MapsAPILoader,
+              private zone: NgZone) {
+  }
 
   ngOnInit() {
   }
